d05faf62879802a339d5cdd4474085ec763a3bd6
Abdelaziz Hany <abdelazizh@alshahin.local>
2021-10-13 21:13:04 +0200

    add two comment in python file

--- bikeshare.py
+++ bikeshare.py
@@ -5,7 +5,7 @@ import numpy as np
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
-
+#get_filter function
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
@@ -45,7 +45,7 @@ def get_filters():
     print('-'*40)
     return city, month, day
 
-
+# load_data function
 def load_data(city, month, day):
     """
     Loads data for the specified city and filters by month and day if applicable.
